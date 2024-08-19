52aa5abcb5c82f078ad32fcdad1ece3c069a8c21
mohamad hosein <mmdtrni@gmail.com>
2024-08-19 18:05:57 +0330

    Preparation of v3

--- script.js
+++ script.js
@@ -1,66 +1,61 @@
+let resultObj = {
+  won: 0,
+  lost: 0,
+  tie: 0,
+};
+
+let choicesObj = {
+  myMove: "rock",
+  computerMove: "rock",
+};
+
+function displayResult() {
+  // TODO: This function must only show the result on the screen
+  // *HINT*: search about how to use javascript object in the HTML tag
+  console.log(resultObj);
+}
+
+function manipulateResult() {
+  /* TODO: This function should call and manipulate the result (resultObj object) 
+  after every time the game finished */
+}
+
 function pickComputerMove() {
-  let computerMove = "";
   const randomNumber = Math.random();
-  if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerMove = "rock";
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = "paper";
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = "scissors";
+  if (0 <= randomNumber < 1 / 3) {
+    choicesObj.computerMove = "rock";
+  } else if (1 / 3 <= randomNumber < 2 / 3) {
+    choicesObj.computerMove = "paper";
+  } else if (2 / 3 <= randomNumber < 1) {
+    choicesObj.computerMove = "scissors";
   }
-  return computerMove;
 }
 
 function rock() {
-  // TODO: corect this
-  computerMove = pickComputerMove();
-  shakingRocks();
-  myMove = "rock";
-  if (computerMove == "rock") {
-    res = "Tie";
-  } else if (computerMove == "paper") {
-    res = "loose";
-  } else if (computerMove == "scissors") {
-    res = "Win";
-  }
-  // alert(`you picked ${myMove}. computer picked ${computerMove}. ${res}`);
+  // TODO: complete this function again with new structure
 }
 
 function paper() {
-  // TODO: corect this
-  computerMove = pickComputerMove();
-  shakingRocks();
-  myMove = "paper";
-  if (computerMove === "rock") {
-    res = "Win";
-  } else if (computerMove === "paper") {
-    res = "Tie";
-  } else if (computerMove === "scissors") {
-    res = "loose";
-  }
-  // alert(`you picked ${myMove}. computer picked ${computerMove}. ${res}`);
+  // TODO: complete this function again with new structure
 }
 
 function scissors() {
-  // TODO: corect this
-  computerMove = pickComputerMove();
-  shakingRocks();
-  myMove = "scissors";
-  if (computerMove === "rock") {
-    res = "loose";
-  } else if (computerMove === "paper") {
-    res = "Win";
-  } else if (computerMove === "scissors") {
-    res = "Tie";
-  }
-  // alert(`you picked ${myMove}. computer picked ${computerMove}. ${res}`);
+  // TODO: complete this function again with new structure
 }
 
 function shakingRocks() {
-  // TODO: corect this
-  // TODO: When this function has called it must show two rock animation shaking for start
+  // TODO: When this function called it must show two rock animation shaking for start
   const yourChoice = document.getElementById("your-choice");
   const computerChoice = document.getElementById("computer-choice");
   console.log(yourChoice);
   console.log(computerChoice);
+}
+
+function doAnimation() {
+  /*
+    TODO: When this function called, two hands should move toward eachother
+    and call shakingRocks function and then the hands should convert to the choices.
+    Your hand (the left one) shout convert to the your choice and the same way for computer
+    Its's going to become similar the actual rock paper scissors game in real
+  */
 }
